8336c9c35861135e272b5889576418992ed04b52
Matthew Hamel <matthamel321@gmail.com>
2022-03-10 13:13:37 -0800

    TESTING ANIMATION

--- src/content/Home.js
+++ src/content/Home.js
@@ -11,11 +11,20 @@ import Paragraph from '../component/Paragraph';
 
 import { testContent, testTitle } from '../strings';
 
+import content from "../strings.json"
+import {motion} from "framer-motion"
+
 
 export default function Home(){
 
         return(
             <div>
+                <motion.div
+                    style={{width:"100px", height:"100px", backgroundColor:"aliceblue"}}
+                    initial={{x:0}}
+                    animate={{x:"90vw"}}
+                    transition={{duration: 2}}
+                />
                 <img src={headImg} id="headImg" />
                 <h1 id='headText'>Matthew Hamel</h1>
 
@@ -25,11 +34,11 @@ export default function Home(){
                 <Navbar />
 
                 <div className="contentBack">
-                        <Paragraph title="Test" content={testContent}/>
-                        <Paragraph title="Test" content={testContent}/>
-                        <Paragraph title="Test" content={testContent}/>
-                        <Paragraph title="Test" content={testContent}/>
-                        </div>
+                    <h1 id='welcome'>Hello and Welcome!</h1>
+                    <h2 id='intro'>My name is Matthew Hamel. <br/> Thank you for taking the time to look over my personal site.</h2>
+                    <Paragraph title={content.home.overview.title} content={content.home.overview.paragraph}/>
+                    <Paragraph title={content.home.languages.title} content={content.home.languages.paragraph}/>
+                </div>
                     
             </div>
 
